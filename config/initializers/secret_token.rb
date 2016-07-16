c3cfff5eed505fe0097196325bd362e0796f1fd9
Filip Dragolov <filip.drag@gmail.com>
2016-07-16 22:35:28 +0300

    downgrade to rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bgmountains::Application.config.secret_key_base = '0b9e795d2e3e45c06ed9dd45f8d30b7eb915e6271c747c88f919977c775d17e56109e0fda8ffc7d5cbe30b1e6751a216424686404b676914d34bb09acbfa58b1'
